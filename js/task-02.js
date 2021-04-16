e652700798ee5be3096e8582a0961608141b04ec
Vladyslav <vladtravel2015@gmail.com>
2021-04-16 15:21:40 +0300

    задача два

--- js/task-02.js
+++ js/task-02.js
@@ -1,28 +1,12 @@
-const ingredients = ['Картошка', 'Грибы', 'Чеснок', 'Помидоры', 'Зелень', 'Приправы'];
-
 const ulElement = document.querySelector('#ingredients');
 
-const liElement1 = document.createElement('li');
-liElement1.textContent = 'Картошка';
+const ingredients = ['Картошка', 'Грибы', 'Чеснок', 'Помидоры', 'Зелень', 'Приправы'];
 
-const liElement2 = document.createElement('li');
-liElement2.textContent = 'Грибы';
+const liIngredients = ingredients.map(liIngredient => {
+  const element = document.createElement('li');
+  element.textContent = liIngredient;
+  return element;
+});
+console.log(liIngredients);
 
-const liElement3 = document.createElement('li');
-liElement3.textContent = 'Чеснок';
-
-const liElement4 = document.createElement('li');
-liElement4.textContent = 'Помидоры';
-
-const liElement5 = document.createElement('li');
-liElement5.textContent = 'Зелень';
-
-const liElement6 = document.createElement('li');
-liElement6.textContent = 'Приправы';
-
-ulElement.append(liElement1, liElement2, liElement3, liElement4, liElement5, liElement6);
-
-console.log(ulElement);
-// Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li,
-// после чего вставит все li за одну операцию в список ul.ingredients.
-// Для создания DOM - узлов используй document.createElement().
+ulElement.append(...liIngredients);
